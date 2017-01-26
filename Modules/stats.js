3494492f8c89334af5dd149888f3a6dd9e474160
Gregory <gregpappas9@gmail.com>
2017-01-26 05:04:06 -0600

    UPDATED ALL THE THINGS

--- Modules/stats.js
+++ Modules/stats.js
@@ -9,22 +9,67 @@ module.exports = {
     handler: function (message, command, params, config, data) {
         if (this.handles.indexOf(command) != -1) {
             if (command == "stats" || command == "botinfo" || command == "botstats" || command == "info") {
-                this.bot(message, data.client, data.Discord, data.prefix);
+                this.bot(message, data.client, data.Discord, data.prefix, data.isTest);
             }
         }
     },
     handles: ["stats", "botinfo", "botstats", "info"],
     helpMessage: "**Statistics for bot**: `stats`\n",
-    bot: function (message, Client, Discord, prefix) {
-        message.channel.sendMessage(
-			"Casper Bot stats and info (beta version)\n" +
-			" - Name: " + Client.user.username + "#" + Client.user.discriminator + " | " + Client.user.id + "\n" +
-			" - Prefix: " + prefix + "\n" +
-			" - Library Version: " + Discord.version + "\n" +
-			"Other Stats\n" +
-			" - Users: " + Client.users.size + "\n" +
-			" - Guilds: " + Client.guilds.size + "\n" +
-            " - Created by: BlaCoiso#2018, Greg#5821, Samuel#1101, gotkeyzjr#6283"
-		);
+    help: function (command) {
+        if (command == "stats" || command == "botinfo" || command == "botstats" || command == "info") {
+            return ["Gives the bot's stats.", ""];
+        } else {
+            return null;
+        }
+    },
+    bot: function (message, Client, Discord, prefix, test) {
+        try {
+            var stats = {
+                color: 15113758,
+                url: "https://discordapp.com/oauth2/authorize?client_id=240498925189070848&scope=bot&permissions=268823574",
+                author: {
+                    name: Client.user.username + " Stats and Info",
+                    icon_url: Client.user.displayAvatarURL,
+                    url: "https://discordapp.com/oauth2/authorize?client_id=240498925189070848&scope=bot&permissions=268823574"
+                },
+                fields: [
+                  {
+                      name: 'Prefix',
+                      value: prefix,
+                      inline: true
+                  },
+                  {
+                      name: 'Library Version',
+                      value: Discord.version,
+                      inline: true
+                  },
+                  {
+                      name: 'Servers',
+                      value: Client.guilds.size,
+                      inline: true
+                  },
+                  {
+                      name: 'Users',
+                      value: Client.users.size,
+                      inline: true
+                  },
+                  {
+                      name: 'Created by',
+                      value: "BlaCoiso#2018, Greg#5821, gotkeyzjr#6283, Lego#5763, Samuel#1101",
+                      inline: false
+                  }, {
+                      name: "Test Version",
+                      value: test ? "Yes" : "No",
+                      inline: true
+                  }
+                ],
+                thumbnail: {
+                    url: Client.user.avatarURL
+                }
+            };
+            message.channel.sendEmbed(stats);
+        } catch (e) {
+            throw e;
+        }
     }
 }
