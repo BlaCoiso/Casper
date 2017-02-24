644ea3199a4944df093abbbe5fbae26c893c12ec
ItsJordan <ItsJordan@users.noreply.github.com>
2017-02-23 21:36:04 -0500

    Update bootup.js

--- bootup.js
+++ bootup.js
@@ -1,16 +1,7 @@
-var Manager = null;
-
 // Main File Systems
 // This file should MOST LIKELY remain UNEDITED
 //  - Exemption: Loading CORE FILE
 
-function init() {
-    Manager.enable();
-}
+const Manager = require('./Managers/core.js');
 
-function loadRequires() {
-    Manager = require('./Managers/core.js'); // Core Manager
-}
-
-loadRequires();
-init();
+Manager.enable();
